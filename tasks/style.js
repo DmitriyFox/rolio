4a87b5b9279ececfc46835d3194be6c4a575feab
DmitriyFox <gazpress_dima@mail.ru>
2022-01-04 17:45:53 +0500

    start pack and folder rolio.

---
+++ tasks/style.js
@@ -0,0 +1,11 @@
+const {
+    src,
+    dest
+} = require('gulp');
+const sass = require('gulp-sass');
+const requireDir = require('require-dir');
+const 
+
+module.exports = function style () {
+    
+}
