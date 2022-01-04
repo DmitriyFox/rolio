1f14afc101a05f6cbbd87ae7b183591989389d06
DmitriyFox <gazpress_dima@mail.ru>
2022-01-04 20:51:42 +0500

    Создал tasks "style.js".

--- tasks/style.js
+++ tasks/style.js
@@ -4,7 +4,12 @@ const {
 } = require('gulp');
 const sass = require('gulp-sass');
 const requireDir = require('require-dir');
-const 
+const bulk = require('gulp-sass-bulk-importer');
+const prefix = require('gulp-autoprefixer');
+const clean  = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const map = require('gulp-sourcemaps');
+const bs = require('browser-sync');
 
 module.exports = function style () {
     
